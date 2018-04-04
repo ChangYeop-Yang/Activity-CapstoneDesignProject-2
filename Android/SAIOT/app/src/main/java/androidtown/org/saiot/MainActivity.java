8e6cca5a768cbc6b96db9647fe1ea9224599cf88
Eiseok Jung <jesboom@naver.com>
2018-04-04 20:32:26 +0900

    [#2] Update Android 2018.04.04. 20:32

--- Android/SAIOT/app/src/main/java/androidtown/org/saiot/MainActivity.java
+++ Android/SAIOT/app/src/main/java/androidtown/org/saiot/MainActivity.java
@@ -1,21 +1,22 @@
 package androidtown.org.saiot;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
 
     LineChart chart;
@@ -29,15 +30,98 @@ public class MainActivity extends AppCompatActivity {
     LineData lineData;
 
 
+
+    //bottom tab 4.4 14:00
+    private HomeFragment homeFragment;
+    private RmoteFragment rmoteFragment;
+    private OutsideFragment outsideFragment;
+    private SettingActivity settingActivity;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-        threadStart();
+        //init();
+        //threadStart();
+
+        //4.4 하단바
+        homeFragment = new HomeFragment();
+        rmoteFragment = new RmoteFragment();
+        outsideFragment = new OutsideFragment();
+        settingActivity = new SettingActivity();
+
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                if(tabId == R.id.tab_home){
+                    transaction.replace(R.id.contentContainer, homeFragment).commit();
+                }
+                else if(tabId == R.id.tab_remote){
+                    transaction.replace(R.id.contentContainer, rmoteFragment).commit();
+                }
+                else if(tabId ==R.id.tab_outside){
+                    transaction.replace(R.id.contentContainer, outsideFragment).commit();
+                }
+                else if (tabId == R.id.tab_setting){
+                    Intent intent4=new Intent(MainActivity.this,SettingActivity.class);
+                    startActivity(intent4);
+                }
+            }
+        });
+
+/*
+        //button click listener
+        Button.OnClickListener onClickListener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                switch (view.getId()) {
+                    case R.id.homebtn :
+                        Intent intent1=new Intent(MainActivity.this,MainActivity.class);
+                        startActivity(intent1);
+                        break ;
+                    case R.id.remotebtn :
+                        Intent intent2=new Intent(MainActivity.this,RemoteActivity.class);
+                        startActivity(intent2);
+                        break ;
+                    case R.id.outsidebtn :
+                        Intent intent3=new Intent(MainActivity.this,OutsideActivity.class);
+                        startActivity(intent3);
+                        break ;
+                    case R.id.settingbtn :
+                        Intent intent4=new Intent(MainActivity.this,SettingActivity.class);
+                        startActivity(intent4);
+                        break ;
+                }
+            }
+        } ;
+        Button homebtn = (Button) findViewById(R.id.homebtn) ;
+        homebtn.setOnClickListener(onClickListener) ;
+        Button remotebtn = (Button) findViewById(R.id.remotebtn) ;
+        remotebtn.setOnClickListener(onClickListener) ;
+        Button outsidebtn = (Button) findViewById(R.id.outsidebtn) ;
+        outsidebtn.setOnClickListener(onClickListener) ;
+        Button settingbtn = (Button) findViewById(R.id.settingbtn) ;
+        settingbtn.setOnClickListener(onClickListener);
+
+*/
     }
 
 
+    public void initFragment(){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.contentContainer, homeFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+/*
     private void init() {
         chart = (LineChart) findViewById(R.id.chart2);
         chartInit();
@@ -111,4 +195,5 @@ public class MainActivity extends AppCompatActivity {
         thread.setDaemon(true);
         thread.start();
     }
+    */
 }
