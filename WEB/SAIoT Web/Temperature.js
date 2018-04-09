665a6384e180bf5f3f1c8ae9f7d323ae201eaa23
COLDY-PC\HOME <saylove4511@naver.com>
2018-04-10 01:59:00 +0900

    [#3] Update 2018.04.10. 01:58:13 Change Project to Use WebStome

---
+++ WEB/SAIoT Web/Temperature.js
@@ -0,0 +1,30 @@
+﻿var temperature = 0;
+
+module.exports = {
+
+	setData: function (req, res) {
+		/*
+		if (message.type === 'utf8') {
+			return message.utf8Data;
+		}
+		else if (message.type === 'binary') {
+			return message.binaryData;
+		}
+		else
+			return message;
+		*/
+
+	},
+
+	getData: function (req, res) {
+		var temperatureObj = {
+			"temperature": temperature
+		}
+		var temperatureStr = JSON.stringify(noizeObj);
+
+		console.log(temperatureStr);
+		res.send(temperature)
+		res.end();
+	}
+
+}
