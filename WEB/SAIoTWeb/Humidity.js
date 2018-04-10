7bddea6d3ea9d3ca207090a966c01101d7941dd9
COLDY-PC\HOME <saylove4511@naver.com>
2018-04-10 11:55:23 +0900

    [#3] Update 2018.04.10 11:54:57 Reupload WebServer

---
+++ WEB/SAIoTWeb/Humidity.js
@@ -0,0 +1,30 @@
+﻿var humidity = 0;
+
+module.exports = {
+
+	setData: function (req, res) {
+		/*
+		if (message.type === 'utf8') {
+			return message.utf8Data;
+		}
+		else if (message.type === 'binary') {
+			return message.binaryData;
+		}
+		else
+			return message;
+		*/
+
+	},
+
+	getData: function (req, res) {
+		var humidityObj = {
+			"humidity": humidity
+		}
+		var humidityStr = JSON.stringify(humidityObj);
+
+		console.log(humidityStr);
+		res.send(humidityStr)
+		res.end();
+	}
+
+}
